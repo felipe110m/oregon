13ab54c1ef60b8b9edcd33f05416933681841a0f
Felipe Mesquita <felipemesqui740@gmail.com>
2026-06-27 16:54:27 -0300

    Setup inicial: home e login pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+﻿export default function LoginPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>OREGON - Login</h1>
+      <p>Página de login funcionando!</p>
+    </div>
+  );
+}
